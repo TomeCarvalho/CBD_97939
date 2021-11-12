4ab35ed2452905baeaa9720a5236784003877512
Tomé Carvalho <tomecarvalho@ua.pt>
2021-11-12 11:59:38 +0000

    2.4 address bugfix

--- lab2/lab2_4/src/main/java/Main.java
+++ lab2/lab2_4/src/main/java/Main.java
@@ -1,9 +1,6 @@
 import com.mongodb.MongoException;
 import com.mongodb.client.*;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Indexes;
-import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.client.model.Updates;
+import com.mongodb.client.model.*;
 import com.mongodb.client.result.UpdateResult;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
@@ -21,10 +18,15 @@ public class Main {
             MongoDatabase cbd = mongoClient.getDatabase("cbd");
             testConnection(cbd);
             MongoCollection<Document> restaurants = cbd.getCollection("restaurants");
-            addTestRestaurant(restaurants);
-            add2Restaurants(restaurants);
-            updateRestaurant(restaurants);
-            findRestaurants(restaurants);
+//            addTestRestaurant(restaurants);
+//            add2Restaurants(restaurants);
+//            updateRestaurant(restaurants);
+//            findRestaurants(restaurants);
+//            query1(restaurants);
+//            query2(restaurants);
+            query3(restaurants);
+//            query4(restaurants);
+//            query5(restaurants);
         }
     }
 
@@ -32,12 +34,12 @@ public class Main {
         try {
             restaurants.insertOne(
                     new Document("_id", new ObjectId())
-                            .append("address", Arrays.asList(
-                                    new Document("building", "1234"),
-                                    new Document("coord", Arrays.asList(0, 0)),
-                                    new Document("rua", "Test Street"),
-                                    new Document("zipcode", "12345")
-                            ))
+                            .append("address",
+                                    new Document("building", "1234")
+                                            .append("coord", Arrays.asList(0, 0))
+                                            .append("rua", "Test Street")
+                                            .append("zipcode", "12345")
+                            )
                             .append("localidade", "Test Location")
                             .append("gastronomia", "Test Gastronomy")
                             .append("grades", Arrays.asList(
@@ -70,12 +72,12 @@ public class Main {
             restaurants.insertMany(
                     Arrays.asList(
                             new Document("_id", new ObjectId())
-                                    .append("address", Arrays.asList(
-                                            new Document("building", "1234"),
-                                            new Document("coord", Arrays.asList(0, 0)),
-                                            new Document("rua", "Test Street"),
-                                            new Document("zipcode", "12345")
-                                    ))
+                                    .append("address",
+                                            new Document("building", "1234")
+                                                    .append("coord", Arrays.asList(0, 0))
+                                                    .append("rua", "Test Street")
+                                                    .append("zipcode", "12345")
+                                    )
                                     .append("localidade", "Test Location")
                                     .append("gastronomia", "Test Gastronomy")
                                     .append("grades", Arrays.asList(
@@ -98,12 +100,12 @@ public class Main {
                                     .append("nome", "Insert Many 1")
                                     .append("restaurant_id", "1"),
                             new Document("_id", new ObjectId())
-                                    .append("address", Arrays.asList(
-                                            new Document("building", "1234"),
-                                            new Document("coord", Arrays.asList(0, 0)),
-                                            new Document("rua", "Test Street"),
-                                            new Document("zipcode", "12345")
-                                    ))
+                                    .append("address",
+                                            new Document("building", "1234")
+                                                    .append("coord", Arrays.asList(0, 0))
+                                                    .append("rua", "Test Street")
+                                                    .append("zipcode", "12345")
+                                    )
                                     .append("localidade", "Test Location")
                                     .append("gastronomia", "Test Gastronomy")
                                     .append("grades", Arrays.asList(
@@ -137,7 +139,7 @@ public class Main {
         Bson updates = Updates.combine(Updates.set("nome", "Updated Test Name"));
         UpdateOptions options = new UpdateOptions().upsert(true);
         try {
-            UpdateResult result = restaurants.updateOne(query, updates, options);
+            UpdateResult result = restaurants.updateMany(query, updates, options);
             System.out.println("Modified document count: " + result.getModifiedCount());
             // only contains a value when an upsert is performed
             System.out.println("Upserted id: " + result.getUpsertedId());
@@ -153,7 +155,9 @@ public class Main {
     }
 
     private static void createIndexes(MongoCollection<Document> restaurants) {
-        restaurants.createIndex(Indexes.text("localidade"));
+        restaurants.createIndex(Indexes.ascending("localidade"));
+        restaurants.createIndex(Indexes.ascending("gastronomia"));
+        restaurants.createIndex(Indexes.text("nome"));
     }
 
     private static boolean testConnection(MongoDatabase database) {
@@ -166,5 +170,56 @@ public class Main {
             System.err.println("An error occurred while attempting to run a command: " + me);
             return false;
         }
+    }
+
+    // c)
+
+    // 1. Liste todos os documentos da coleção.
+    private static void query1(MongoCollection<Document> restaurants) {
+        FindIterable<Document> docsIterable = restaurants.find();
+        System.out.println("Query 1");
+        for (Document doc : docsIterable)
+            System.out.println(doc.toJson());
+    }
+
+    // 2. Apresente os campos restaurant_id, nome, localidade e gastronomia para todos os
+    //    documentos da coleção.
+    private static void query2(MongoCollection<Document> restaurants) {
+        FindIterable<Document> docsIterable = restaurants.find().projection(
+                Projections.fields(
+                        Projections.include("restaurant_id", "nome", "localidade", "gastronomia")
+                )
+        );
+        System.out.println("Query 2");
+        for (Document doc : docsIterable)
+            System.out.println(doc.toJson());
+    }
+
+    // 3. Apresente os campos restaurant_id, nome, localidade e código postal (zipcode), mas
+    // exclua o campo _id de todos os documentos da coleção.
+    private static void query3(MongoCollection<Document> restaurants) {
+        FindIterable<Document> docsIterable = restaurants.find().projection(
+                Projections.fields(
+                        Projections.include("restaurant_id", "nome", "localidade", "gastronomia", "address.zipcode"),
+                        Projections.exclude("_id")
+                )
+        );
+        System.out.println("Query 3");
+        for (Document doc : docsIterable)
+            System.out.println(doc.toJson());
+    }
+
+    // 4. Indique o total de restaurantes localizados no Bronx
+    private static void query4(MongoCollection<Document> restaurants) {
+        System.out.println("Query 4\nNumber of restaurants located in the Bronx: " +
+                restaurants.countDocuments((Filters.eq("localidade", "Bronx"))));
+    }
+
+    // 5. Apresente os primeiros 15 restaurantes localizados no Bronx, ordenados por
+    //    ordem crescente de nome.
+    private static void query5(MongoCollection<Document> restaurants) {
+        FindIterable<Document> docsIterable = restaurants.find(
+                Filters.eq("localidade", "Bronx")
+        ).limit(15).sort(Sorts.ascending("nome"));
     }
 }
